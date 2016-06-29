7834eeda4071a18debdd6ae09ad72180f0fe7995
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-06-29 18:28:39 +0200

    Displaying toasts on click!

--- app/src/main/java/cz/tomasfejfar/tomasfejfarportfolio/Portfolio.java
+++ app/src/main/java/cz/tomasfejfar/tomasfejfarportfolio/Portfolio.java
@@ -1,7 +1,11 @@
 package cz.tomasfejfar.tomasfejfarportfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Portfolio extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Portfolio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
+    }
+
+    public void sendMessage(View view) {
+        CharSequence buttonId = "";
+        if (view instanceof Button) {
+            buttonId = ((Button) view).getText();
+        }
+        Context context = getApplicationContext();
+        CharSequence text = "This will display the " + buttonId + " app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
