cc08045daa0111874ab27e108c9aa7a9f39e4c3f
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-06-29 18:06:23 +0200

    Initial commit

---
+++ app/src/main/java/cz/tomasfejfar/tomasfejfarportfolio/Portfolio.java
@@ -0,0 +1,13 @@
+package cz.tomasfejfar.tomasfejfarportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Portfolio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portfolio);
+    }
+}
